cba866d037461e4c76968406160595702e750280
Abhi Rehal <rehalabhi@gmail.com>
2025-06-24 23:27:50 +1200

    Title button will now reload the page to reset the state

--- src/components/buttons/title-button.js
+++ src/components/buttons/title-button.js
@@ -1,12 +1,8 @@
 import './buttons.css';
 
 export function TitleButton() {
-  function handleClick() {
-    console.log(`This will take you back to the initial state`);
-  }
-
   return (
-    <button className="title-button" onClick={handleClick}>
+    <button className="title-button" onClick={() => window.location.reload()}>
       <span className="title-button-text">Pathfinding Visualizer</span>
     </button>
   );
