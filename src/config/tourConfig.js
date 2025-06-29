7d2cf23e026b4662178ca49df76848b264344aad
Abhi Rehal <rehalabhi@gmail.com>
2025-06-29 20:21:01 +1200

    Added pathfinding-demo.gif and added it to the tour explaining what pathfinding is

--- src/config/tourConfig.js
+++ src/config/tourConfig.js
@@ -1,6 +1,7 @@
 export const tourSteps = [
   {
     element: '.title-button',
+    disableActiveInteraction: true,
     popover: {
       description: 'Feel free to skip this tutorial. You can bring it up at anytime by pressing this button.',
       align: 'center',
@@ -14,7 +15,16 @@ export const tourSteps = [
     }
   },
   {
+    element: null,
+    popover: {
+      title: "What's a pathfinding algorithm?",
+      description:
+        'Pathfinding algorithms do as the name suggests and determine how to get from point A to B.</br></br><img src="/assets/pathfinding-demo.gif" alt="Pathfinding demo"/>'
+    }
+  },
+  {
     element: '.generate-maze-dropdown',
+    disableActiveInteraction: true,
     popover: {
       description: 'Select an algorithm you would like to use to generate a maze.',
       align: 'center',
@@ -29,6 +39,7 @@ export const tourSteps = [
   },
   {
     element: '.generate-maze-button',
+    disableActiveInteraction: true,
     popover: {
       description:
         'You can then use this button to generate the maze.</br></br>Or you can even draw your own by clicking and dragging your mouse on the grid!</br></br><img src="/assets/walls.gif" alt="Wall placing gif"/>',
@@ -44,6 +55,7 @@ export const tourSteps = [
   },
   {
     element: '.algorithm-dropdown',
+    disableActiveInteraction: true,
     popover: {
       description: "Select the pathfinding algorithm you'd like to visualize...",
       align: 'center',
@@ -58,6 +70,7 @@ export const tourSteps = [
   },
   {
     element: '.visualize-button',
+    disableActiveInteraction: true,
     popover: {
       description: 'and visualize it!',
       align: 'center',
@@ -72,6 +85,7 @@ export const tourSteps = [
   },
   {
     element: '.sidebar-button',
+    disableActiveInteraction: true,
     popover: {
       description: 'Advanced settings can be found here.',
       align: 'center',
