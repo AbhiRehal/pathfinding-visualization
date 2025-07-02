b4c1bda59cf010deb199ddf2e117661e6925aeef
Abhi Rehal <rehalabhi@gmail.com>
2025-07-03 11:20:08 +1200

    Deleted unused files

--- src/components/checkbox.js
+++
@@ -1,8 +0,0 @@
-export function Checkbox({ isChecked, setChecked }) {
-  return (
-    <>
-      <input id="wall-checkbox" type="checkbox" checked={isChecked} onChange={e => setChecked(e.target.checked)}></input>
-      <label for="wall-checkbox">Set walls</label>
-    </>
-  );
-}
--- src/components/set-visited-checkbox.js
+++
@@ -1,8 +0,0 @@
-export function Visited({ isChecked, setChecked }) {
-  return (
-    <>
-      <input id="visited-checkbox" type="checkbox" checked={isChecked} onChange={e => setChecked(e.target.checked)}></input>
-      <label for="visited-checkbox">Set visited</label>
-    </>
-  );
-}
